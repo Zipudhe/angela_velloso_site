adf66dd02b114250b0b20b2e119965581414e6ae
lucas.ramalho <ramalhodev.lucas@gmail.com>
2024-09-30 21:35:27 -0300

    feat: update base page css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
 import { ApplicationTheme } from "@/libs/theme";
 import styles from "./page.module.css";
 import { TypographParagraph } from '../components/Typograph'
+import { AboutSection } from '../components/AboutSection/AboutSection'
 
 export default function Home() {
   return (
     <ApplicationTheme>
       <div className={styles.page}>
         <main className={styles.main}>
-          <TypographParagraph style={{ fontWeight: 'bold' }} as="p" > Main </TypographParagraph>
+          <AboutSection />
         </main>
         <footer className={styles.footer}>
           <TypographParagraph as="h1" > Footer </TypographParagraph>
