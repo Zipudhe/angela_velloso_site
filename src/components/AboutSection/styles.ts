fdb8d8e44137989b65df62a0e521184fd8f3203d
lucas.ramalho <ramalhodev.lucas@gmail.com>
2024-09-30 21:35:49 -0300

    feat: create About Section

---
+++ src/components/AboutSection/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  width: 100%;
+  min-width: 1000px;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  > p {
+    text-align: justify;
+  }
+
+  > h1 {
+    align-self: center;
+  }
+`
