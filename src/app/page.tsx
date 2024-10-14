3d0d47992a9a6503a7a0475bcfd176c21ce7cd8c
lucas.ramalho <ramalhodev.lucas@gmail.com>
2024-10-13 22:39:52 -0300

    feat: creates media sections with links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ApplicationTheme } from "@/libs/theme";
 import styles from "./page.module.css";
 import { TypographParagraph } from '../components/Typograph'
 import { AboutSection } from '../components/AboutSection/AboutSection'
+import { MediaSection } from '../components/MediaSection/MediaSection'
 
 export default function Home() {
   return (
@@ -11,9 +12,11 @@ export default function Home() {
       <div className={styles.page}>
         <main className={styles.main}>
           <AboutSection />
+          <MediaSection />
         </main>
         <footer className={styles.footer}>
           <TypographParagraph as="h1" > Footer </TypographParagraph>
+          <TypographParagraph as={"span"} > icons by <a target="_blank" href="https://icons8.com">Icons8</a> </TypographParagraph>
         </footer>
       </div>
     </ApplicationTheme>
---
+++ src/components/MediaSection/MediaSection.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import React, { FC } from 'react';
+import { IconsWrapper } from './styles'
+import { Icon } from '../Icon/Icon'
+
+import InstagramIcon from '../../assets/icons/instagram.svg'
+import SpotifyIcon from '../../assets/icons/spotify.svg'
+import DeezerIcon from '../../assets/icons/deezer.svg'
+import SoundcloudIcon from '../../assets/icons/soundcloud.svg'
+import ItunesIcon from '../../assets/icons/itunes.svg'
+import YoutubeIcon from '../../assets/icons/youtube.svg'
+
+
+export const MediaSection: FC = () => {
+
+  return (
+    <IconsWrapper>
+      <a target='_blank' href='https://www.instagram.com/angela_velloso/' >
+        <Icon path={InstagramIcon} />
+      </a>
+      <a target='_black' href="https://open.spotify.com/artist/42GPYWdoneNvorglZQDPzo?si=N-37bDu4Qh-KfLudgTs-Bg&nd=1&dlsi=c215c1d66f6a48a5">
+        <Icon path={SpotifyIcon} />
+      </a>
+      <a target='_blank' href='https://www.deezer.com/us/artist/74051492'>
+        <Icon path={DeezerIcon} />
+      </a>
+      <a target="_blank" href='https://soundcloud.com/angela-velloso'>
+        <Icon path={SoundcloudIcon} />
+      </a>
+      <a target='_blank' href="https://music.apple.com/br/artist/angela-velloso/1480023629">
+        <Icon path={ItunesIcon} />
+      </a>
+      <a target='_blank' href='https://www.youtube.com/channel/UCOHbKQpeujl9IK0BlxPGmUg'>
+        <Icon path={YoutubeIcon} />
+      </a>
+    </IconsWrapper>
+  )
+}
