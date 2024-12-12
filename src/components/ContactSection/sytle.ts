87349f20a4f392e8ade9010799dcee1949a11d0a
lucas.ramalho <ramalhodev.lucas@gmail.com>
2024-12-12 11:37:01 -0300

    feature: turn contact section responsive

--- src/components/ContactSection/sytle.ts
+++ src/components/ContactSection/sytle.ts
@@ -4,4 +4,10 @@ export const ContactWrapper = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-evenly;
+
+  @media(max-width: 900px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+  }
 `
